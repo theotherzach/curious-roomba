44ca50b36e387888a29fdd93757f779a329d1e8b
Zach <briggszj@gmail.com>
2014-06-13 11:39:26 -0500

    Add basic predictive behavior to Cell.

--- app/js/roomba/cell.js
+++ app/js/roomba/cell.js
@@ -1,14 +1,42 @@
-function Cell() {
+function Cell(sdr) {
   "use strict";
   var self = this;
   var _state = "off";
+  var _harbingers = [];
+  var _isNewPrediction = false;
+
+  function tick() {
+    if (_state === "on") {
+      turnOff();
+    } else if (_(_harbingers).union(sdr.activeCells()).length == sdr.activeCells().length) {
+
+      if (_harbingers.length > 0) {
+
+        if (_isNewPrediction) {
+          sdr.predicted();
+          _isNewPrediction = false;
+        }
+
+      turnOn();
+      }
+    }
+
+    return self;
+  }
 
   function state() {
     return _state;
   }
 
   function turnOn() {
+
     _state = "on";
+
+    if (_harbingers.length === 0 ) {
+      _harbingers = _(sdr.activeCells()).sample(10);
+      _isNewPrediction = true;
+    }
+
     return self;
   }
 
@@ -17,6 +45,7 @@ function Cell() {
     return self;
   }
 
+  self.tick = tick;
   self.turnOff = turnOff;
   self.turnOn = turnOn;
   self.state = state;
--- spec/roomba/cell-spec.js
+++ spec/roomba/cell-spec.js
@@ -1,8 +1,13 @@
 describe("cell", function () {
-  var cell
+  var cell, sdr = null;
 
   beforeEach(function () {
-    cell = new Cell();
+    sdr = {
+      activeCells: function () { return []; },
+      predicted: function () {}
+    }
+
+    cell = new Cell(sdr);
   });
 
   it("state is off by default", function () {
@@ -13,16 +18,49 @@ describe("cell", function () {
     expect(cell.turnOn().state()).toBe("on");
   });
 
-  xit("samples the previous step's SDR when on", function () {
-  });
-
-  xit("predicts whether it is about to turn on again.", function () {
-  });
-
-  xit("transitions to expected on subsequent succesful predictions", function () {
+  it("checks the previous step's SDR when on", function () {
+    spyOn(sdr, "activeCells").andReturn(_(_.range(200)).sample(40));
+    cell.turnOn();
+    expect(sdr.activeCells).toHaveBeenCalled();
   });
 
   describe("predictions", function () {
+    beforeEach(function () {
+      spyOn(sdr, "activeCells").andReturn(_(_.range(2000)).sample(40));
+      cell.turnOn().tick();
+    });
+
+    it("starts at OFF", function () {
+      expect(cell.state()).toBe("off");
+    });
+
+    it("predicts whether it is about to turn on again.", function () {
+      spyOn(sdr, "predicted");
+      cell.tick();
+      expect(sdr.predicted).toHaveBeenCalled();
+    });
+
+    it("gets bored with the same prediction", function () {
+      cell.tick().tick()
+      spyOn(sdr, "predicted");
+      cell.tick().tick()
+      expect(sdr.predicted).not.toHaveBeenCalled();
+    });
+
+    it("turns itself on if the conditions are right", function () {
+      cell.tick();
+      expect(cell.state()).toBe("on");
+    });
+
+    it("stays off when the pattern is new", function () {
+      sdr.activeCells = function () { };
+      spyOn(sdr, "activeCells").andReturn(_(_.range(2000)).sample(40));
+      cell.tick();
+      expect(cell.state()).toBe("off");
+      cell.tick();
+      expect(cell.state()).toBe("off");
+    });
+
     xit("makes a prediction based on a sub-sample of the previously active cells", function () {
     });
 
