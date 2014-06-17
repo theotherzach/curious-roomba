f3487d2dea8b30dc0fef69e92572c702f6662e61
Zach <briggszj@gmail.com>
2014-06-16 21:35:09 -0500

    Add nothing spec

---
+++ spec/nothing-spec.js
@@ -0,0 +1,14 @@
+describe("Nothing.", function() {
+  it("Sustains patterns for a long time.");
+  it("Lives above hard wired rules.");
+  it("Does not live without hard wired rules to live above.");
+  it("Takes notice of the output but only generates it indirectly. With a feather's touch.");
+  it("Is born blank.");
+  it("Is ruled by emotions. Emotions are rules.");
+
+  describe("A Pattern", function () {
+    it("is hard to kill. Resilliant.");
+    it("can jump from meatbag to meatbag.");
+    it("is only worth its use.");
+  });
+});
