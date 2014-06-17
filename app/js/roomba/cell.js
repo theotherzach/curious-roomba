4bcd7faf877803c18859614d8c65915e7508e91b
Zach <briggszj@gmail.com>
2014-06-16 23:31:35 -0500

    Finish first pass at cell.js

--- app/js/roomba/cell.js
+++ app/js/roomba/cell.js
@@ -3,14 +3,64 @@ function Cell(neighborhood) {
   var self = this;
 
   self.watchList = [];
+  self.predictive = false;
+  self.state = "off";
+  self.id = makeGuid();
+
+  neighborhood.register(self);
+
 
   self.turnOn = function () {
-    neighborhood.activeCells();
+    self.state = "on";
+
+    neighborhood.alertToOn(self);
+
+    if (self.predictive) {
+    } else {
+      var activeCells = neighborhood.activeLastStep();
+      neighborhood.watch(activeCells, function (cellIds) {
+        self.poll(cellIds);
+      });
+    }
 
     return self;
   };
 
+  self.poll = function (cellIds) {
+    var sampledCells = cellIds.map(function(e, i) {
+      return {
+        id: e,
+        active: neighborhood.isCellActive(e)
+      };
+    });
+
+    predictive = predict(sampledCells);
+    return self;
+  };
+
+  function predict(sampledCells) {
+    return Math.random() < 0.2;
+  }
+
   self.turnOff = function () {
+    self.state = "off";
+    return self;
+  };
+
+  self.turnPrevious = function () {
+    self.state = "previous";
+    return self;
+  };
+
+  self.tick = function(feedFwd) {
+    if(feedFwd) {
+      self.turnOn();
+    } else if(self.state === "on") {
+      self.turnPrevious();
+    } else {
+      self.turnOff();
+    }
+
     return self;
   };
 
--- spec/roomba/cell-spec.js
+++ spec/roomba/cell-spec.js
@@ -2,19 +2,27 @@ describe("A Cell", function () {
   var cell, neighborhood;
   beforeEach(function () {
     neighborhood = {
-      activeCells: jasmine.createStub("activeCells").andReturn([1,2,3]),
+      activeLastStep: jasmine.createSpy("activeLastStep").andReturn([1,2,3]),
+      watch: jasmine.createSpy("watch").andReturn(neighborhood),
+      register: jasmine.createStub("register"),
+      alertToOn: jasmine.createStub("alertToOn"),
+      getId: jasmine.createStub("getId"),
     };
     cell = new Cell(neighborhood);
   });
 
   it("takes notice of other cells when activated", function () {
     cell.turnOn();
-    expect(neighborhood.activeCells).toHaveBeenCalled();
+    expect(neighborhood.activeLastStep).toHaveBeenCalled();
+    expect(neighborhood.watch).toHaveBeenCalled();
   });
 
   it("predicts when it will be activated again by keeping an eye on some of the neighbors that were active last time", function(){
     cell.turnOn().turnOff();
+    cell.tick(true).tick(false).tick(false)
   });
+
+  it("updates reliability markers even if it's not activated");
 
   it("slowly stops paying attention to neighbors that are unreliable");
 
